b87717a2a0fd74ba1f4adfdcbd70e7612cdab21a
Xizi Zhou <43325923+ZhouXizi@users.noreply.github.com>
2020-12-24 20:26:37 -0800

    增加router和vuex

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <button class="button is-primary">测试</button>
+  <Home />
 </template>
 
 <script>
+import Home from "@/views/Home.vue";
+
 export default {
   name: "App",
+  components: {
+    Home,
+  },
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
